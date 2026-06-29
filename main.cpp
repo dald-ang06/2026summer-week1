cb2789c545a6ed3933431e287128179097f54414
dald-ang06 <ldy2566038@ewha.ac.kr>
2026-06-29 02:45:11 +0000

    첫 활동

--- main.cpp
+++ main.cpp
@@ -1,1 +1,37 @@
 
+#include <iostream>
+int main(){
+    // int numOfStudents{30}; //함수로 오인받을 수 있어서, 중괄호를 사용함, 크기는 integer
+    // //numOfStudents=22; 상수는 변경할 수 없음, 변경하고 싶으면 const 지워야함
+    // std::cout<<numOfStudents<<'\n';//'\n' 대신에 std::endl; 써도됨
+    // std::cout<<sizeof(numOfStudents)<<std::endl;
+    // std::cout<<typeid(numOfStudents).name()<<std::endl;
+    // std::cout<<typeid(static_cast<double>(numOfStudents)).name()<<std::endl;//double형이 나온다는 의미로 d가 나옴
+
+    //crtl /
+    // std::cout<<"Enter two integers: ";
+    // int x{}, y{},z{};//여기다 값을 넣을거야!
+    // std::cin>> x >>y;
+
+
+    // // if(x>y)z=x;
+    // // else z=y;
+    // z=x>y?x:y;//삼항연산자
+    // std::cout<<z<<std::endl;
+
+    // std::cout<<"Enter 1,2,or3 ";
+    // int x{};
+    // std::cin>>x;
+    // switch(x){
+    //     case 1:std::cout<<"one\n"; break;
+    //     case 2:std::cout<<"two\n"; break;
+    //     case 3:std::cout<<"three\n"; break;
+    //     default: std::cout<<"unknown\n";//마지막에는 break 있어도 되고 없어도 됨
+    // }
+
+    int i=1;
+    const int end{0};
+    for(i=1;i<=end;++i){}
+    std::cout<<std::endl;
+    return 0;
+}  
